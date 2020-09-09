a77cd1242bba7ef72942f841056737428889981c
Sabrina Mazlan <54005211+sabrinasyazwani97@users.noreply.github.com>
2020-09-09 23:05:56 +0800

    Update imageSegment.py

--- imageSegment.py
+++ imageSegment.py
@@ -2,31 +2,18 @@
 """
 imageSegment.py
 
-@student_name: Nur Sabrina Syazwani binti Mazlan
-@student_id: 1171302745
-
-YOUR WORKING FUNCTION
-
 """
 import cv2
 import numpy as np
 from matplotlib import pyplot as plt
 
-
-input_dir = 'Documents/1171302745_Assignment/dataset/test'
-output_dir = 'Documents/1171302745_Assignment/output'
-
-# you are allowed to import other Python packages above
-##########################
 def segmentImage(img):
     # Inputs
     # img: Input image, a 3D numpy array of row*col*3 in BGR format
     #
     # Output
     # outImg: segmentation image
-    #
-    #########################################################################
-    # ADD YOUR CODE BELOW THIS LINE
+
     original_img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
     filter_img = cv2.medianBlur(original_img,9)
     hsv_img = cv2.cvtColor(filter_img, cv2.COLOR_RGB2HSV)
@@ -48,8 +35,5 @@ def segmentImage(img):
     
     outImg = green_mask + yellow_mask + red_mask + red_mask2
     outImg = np.clip(outImg,0,1)   
-   
-       
-    # END OF YOUR CODE
-    #########################################################################
+
     return outImg
